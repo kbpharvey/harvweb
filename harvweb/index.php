c6d8956fde481af6cf1a873ad1b636d00e503c14
kbpharvey <paul.karenharvey@btinternet.com>
2012-06-29 09:47:15 +0100

    dev switch

--- harvweb/index.php
+++ harvweb/index.php
@@ -1,7 +1,21 @@
 <?php
-echo "harv web welcome";
+// change the following paths if necessary
+$env = $_SERVER['HTTP_HOST'];
+$yii=dirname(__FILE__).'/framework/yii.php';
+if ($env == 'localhost:8080')
+{
+echo 'dev';
+$config=dirname(__FILE__).'/protected/config/main_dev.php';
+}
+else
+{
+$config=dirname(__FILE__).'/protected/config/main.php';
+}
+// remove the following lines when in production mode
+defined('YII_DEBUG') or define('YII_DEBUG',true);
+// specify how many levels of call stack should be shown in each log message
+defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
-echo "lets enjoy a harvey day ;)";
-
-echo "site currently under construction - will be with you shortly";
+require_once($yii);
+Yii::createWebApplication($config)->run();
 ?>
